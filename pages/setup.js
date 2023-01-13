cb7d24b513cf9a6422bfc24525bf438c284c47d2
Java <indead1124@gmail.com>
2023-01-13 17:02:31 -0500

    update data

--- pages/setup.js
+++ pages/setup.js
@@ -5,7 +5,7 @@ const setupTemplate = `
 	<br>
 	<br>## <span class="secondary">Software <3</span>
 	<br>
-	<br>&nbsp&nbsp&nbsp-- Desktop: 
+	<br>&nbsp&nbsp&nbsp-- PC (Windows): 
 	<br>&nbsp&nbsp&nbspNote-taking: <a href="https://obsidian.md" target="_blank" rel="noopener nonreferrer">Obsidian</a>
 	<br>&nbsp&nbsp&nbspNote-taking: <a href="https://slite.com" target="_blank" rel="noopener nonreferrer">Slite</a> (archived)
 	<br>&nbsp&nbsp&nbspDrawing: <a href="https://firealpaca.com/" target="_blank" rel="noopener nonreferrer">FireAlpaca</a>
@@ -14,17 +14,18 @@ const setupTemplate = `
 	<br>&nbsp&nbsp&nbspDev: <a href="https://github.com/microsoft/vscode" target="_blank" rel="noopener nonreferrer">VSCode</a>
 	<br>&nbsp&nbsp&nbspDev: <a href="https://neovim.io/" target="_blank" rel="noopener nonreferrer">Neovim</a> (<a href="https://gist.github.com/Javascript-void0/efc20ccc706ef77ecb42e2a52749011e" target="_blank" rel="noopener nonreferrer">Config</a>)
 	<br>
-	<br>&nbsp&nbsp&nbsp-- Mobile: 
+	<br>&nbsp&nbsp&nbsp-- Mobile (Android): 
 	<br>&nbsp&nbsp&nbspAnime/Manga: <a href="https://github.com/tachiyomiorg/tachiyomi" target="_blank" rel="noopener nonreferrer">Tachiyomi</a> / <a href="https://github.com/jmir1/aniyomi" target="_blank" rel="noopener nonreferrer">Aniyomi</a>
-	<br>&nbsp&nbsp&nbspAnilist Client: <a href="https://play.google.com/store/apps/details?id=com.otraku.app&hl=en_US&gl=US" target="_blank" rel="noopener nonreferrer">Tachiyomi</a>
-	<br>&nbsp&nbsp&nbspYouTube Vanced
+	<br>&nbsp&nbsp&nbspAnilist Client: <a href="https://play.google.com/store/apps/details?id=com.otraku.app&hl=en_US&gl=US" target="_blank" rel="noopener nonreferrer">Otraku</a>
+	<br>&nbsp&nbsp&nbspYouTube Vanced 💪
 	<br>
 	<br>## <span class="secondary">Hardware</span>
 	<br>
 	<br>&nbsp&nbsp&nbspLaptop: Microsoft Surface Pro 6
 	<br>&nbsp&nbsp&nbspPhone: Sony Xperia 5 iii
 	<br>&nbsp&nbsp&nbspAudio: Microsoft Surface Headphones 2
-	<br>&nbsp&nbsp&nbspAudio: KZ DQ6 IEM
+	<br>&nbsp&nbsp&nbspAudio: Moondrop Chu
 	<br>&nbsp&nbsp&nbspAudio: Sony WF-1000XM3 Earbuds
+	<br>&nbsp&nbsp&nbspMonitor: LG Dualup
 	<br>
 `
