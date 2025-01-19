bcf4041d0b784e1bf092348d91ffa20851dccd4c
Java <73376510+Javascript-void0@users.noreply.github.com>
2025-01-19 17:19:35 -0500

    scrollbar drag

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -2,7 +2,46 @@ String.prototype.replaceAt = function(index, replacement) {
     return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
 
+onScrollBar = false;
+function scrollBarMouseDown(event) {
+    onScrollBar = true;
+    mousePosToScrollPos(event)
+}
+
+function scrollBarMouseUp() {
+    onScrollBar = false;
+}
+
+function scrollBarMouseMove(event) {
+    if (!onScrollBar) { return; }
+    mousePosToScrollPos(event)
+}
+
+function mousePosToScrollPos(event) {
+
+    mouseHeightPx = event.clientY;
+    totalHeightPx = rightBorder.clientHeight;
+    offset = rightBorder.getBoundingClientRect().top;
+    if (mouseHeightPx < offset) {
+        mouseHeightPx = 0
+    } else if (mouseHeightPx > offset + totalHeightPx) {
+        mouseHeightPx = totalHeightPx
+    } else {
+        mouseHeightPx -= offset
+    }
+
+    barHeight = barPercent * totalHeightPx;
+    scrollPos = ((mouseHeightPx - barHeight / 2) / totalHeightPx * page.scrollHeight);
+    scrollPos = Math.floor(scrollPos)
+
+    scrollBar(scrollPos)
+}
+
+let barPercent;
+var page;
+
 function updateScrollBar() {
+
     if (currentPage == 'blog') {
         page = document.getElementById('blog-right')
     } else {
@@ -29,4 +68,14 @@ function updateScrollBar() {
         bar = bar.repeat(barHeight)
         rightBorder.innerHTML = rightBorder.innerHTML.replaceAt(rowCharLen * row, bar)
     }
+}
+
+function scrollBar(scrollPos) {
+    if (currentPage == 'blog') {
+        page = document.getElementById('blog-right')
+    } else {
+        page = document.getElementById('page')
+    }
+
+    page.scrollTop = scrollPos;
 }
